68567989fb7cb83b7da8818b16993fb2f0b97ca9
napornapon <na_hahaha@hotmail.com>
2016-09-13 15:59:14 +0700

    Update Users.java

--- lab4_2/src/cpe200/Users.java
+++ lab4_2/src/cpe200/Users.java
@@ -8,42 +8,78 @@ import java.util.ArrayList;
 public class Users {
     public ArrayList<User> userList;
 
+    public Users() {
+        userList= new ArrayList<User>();
+    }
+
     public void addUser(User user)
     {
+        userList.add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        User user = new User();
+        user.setUserName(userName);
+        user.setPassword(password);
+        userList.add(user);
     }
 
     public void deleteUser(User user)
     {
-
+        userList.remove(user);
     }
 
     public boolean exists(User user)
     {
-        return false;
+
+        if(userList.isEmpty()) {
+            return false;
+        }
+        else if(userList.contains(user)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
     }
 
     public boolean usernameExists(String username)
     {
-        return false;
+        if(userList.isEmpty()) {
+            return true;
+        }
+        for (int i=0; i<userList.size(); i++)
+        {
+            if(username == userList.get(i).getUserName())
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     /* This method should return null when the user with username is not in the list */
     public User getUserByUsername(String userName)
     {
+        for (int i=0; i<userList.size(); i++)
+        {
+            if(userName == userList.get(i).getUserName())
+            {
+                return userList.get(i);
+            }
+        }
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return userList.size();
     }
 
     public User[] getUserArray()
     {
-        return null;
+        return userList.toArray(new User[userList.size()]);
     }
 }
