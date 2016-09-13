3c75db2befb89de47d2cad0b77938f125634ba09
napornapon <na_hahaha@hotmail.com>
2016-09-13 16:00:00 +0700

    Update BinaryCalculator.java

--- lab4_1/src/cpe200/BinaryCalculator.java
+++ lab4_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,228 @@
 package cpe200;
 
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
+    private static final String dotREGEX = "[.]";
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+        this.firstOperand = operand;
     }
 
 
     public void setSecondOperand(Operand operand) {
+        this.secondOperand = operand;
     }
 
     public String add() {
-        return null;
+        String St_sum;
+        String first = firstOperand.getOperand();
+        String second = secondOperand.getOperand();
+        Pattern patternf = Pattern.compile(dotREGEX);
+        Matcher mf = patternf.matcher(first);
+        Pattern patterns = Pattern.compile(dotREGEX);
+        Matcher ms= patterns.matcher(second);
+        if(mf.find() || ms.find())
+        {
+            double dfirst = Double.parseDouble(first);
+            double dsecond = Double.parseDouble(second);
+            double sum = dfirst + dsecond;
+            sum = sum*100000;
+            if((sum*10)%10 >= 5 || (sum*10)%10 <= -5) {
+                if(sum>0){sum++; }
+                else {sum--; }
+            }
+            int dec_sum = (int)sum;
+            sum = (double)dec_sum/100000;
+            if((sum*10)%10 == 0) {
+                int hold_sum = (int) sum;
+                St_sum = Integer.toString(hold_sum);
+            }
+            else {
+                St_sum = Double.toString(sum);
+            }
+        }
+        else {
+            int dfirst = Integer.parseInt(first);
+            int dsecond = Integer.parseInt(second);
+            int sum = dfirst + dsecond;
+            St_sum = Integer.toString(sum);
+        }
+
+
+        return St_sum;
     }
 
     public String subtract() {
-        return null;
+        String St_sum;
+        String first = firstOperand.getOperand();
+        String second = secondOperand.getOperand();
+        Pattern patternf = Pattern.compile(dotREGEX);
+        Matcher mf = patternf.matcher(first);
+        Pattern patterns = Pattern.compile(dotREGEX);
+        Matcher ms= patterns.matcher(second);
+        if(mf.find() || ms.find())
+        {
+            double dfirst = Double.parseDouble(first);
+            double dsecond = Double.parseDouble(second);
+            double sum = dfirst - dsecond;
+            sum = sum*100000;
+            if((sum*10)%10 >= 5 || (sum*10)%10 <= -5 && sum!=0) {
+                if(sum>0){sum++; }
+                else {sum--; }
+            }
+            int dec_sum = (int)sum;
+            sum = (double)dec_sum/100000;
+            if((sum*10)%10 == 0) {
+                int hold_sum = (int) sum;
+                St_sum = Integer.toString(hold_sum);
+            }
+            else {
+                St_sum = Double.toString(sum);
+            };
+        }
+        else {
+            int dfirst = Integer.parseInt(first);
+            int dsecond = Integer.parseInt(second);
+            int sum = dfirst - dsecond;
+            St_sum = Integer.toString(sum);
+        }
+
+
+        return St_sum;
     }
 
     public String multiply() {
-        return null;
+        String St_sum;
+        String first = firstOperand.getOperand();
+        String second = secondOperand.getOperand();
+        Pattern patternf = Pattern.compile(dotREGEX);
+        Matcher mf = patternf.matcher(first);
+        Pattern patterns = Pattern.compile(dotREGEX);
+        Matcher ms= patterns.matcher(second);
+        if(mf.find() || ms.find())
+        {
+            double dfirst = Double.parseDouble(first);
+            double dsecond = Double.parseDouble(second);
+            double sum = dfirst * dsecond;
+            sum = sum*100000;
+            if((sum*10)%10 >= 5 || (sum*10)%10 <= -5 && sum!=0) {
+                if(sum>0){sum++; }
+                else {sum--; }
+            }
+            int dec_sum = (int)sum;
+            sum = (double)dec_sum/100000;
+            if((sum*10)%10 == 0) {
+                int hold_sum = (int) sum;
+                St_sum = Integer.toString(hold_sum);
+            }
+            else {
+                St_sum = Double.toString(sum);
+            }
+        }
+        else {
+            int dfirst = Integer.parseInt(first);
+            int dsecond = Integer.parseInt(second);
+            int sum = dfirst * dsecond;
+            St_sum = Integer.toString(sum);
+        }
+
+
+        return St_sum;
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws ArithmeticException {
-        return null;
+        String St_sum;
+        String first = firstOperand.getOperand();
+        String second = secondOperand.getOperand();
+        if(second.equals("0.0")) {
+            int nfirst = Integer.parseInt(first);
+            int nsecond = Integer.parseInt(second);
+            first = Integer.toString(nfirst);
+            second = Integer.toString(nsecond);
+        }
+        Pattern patternf = Pattern.compile(dotREGEX);
+        Matcher mf = patternf.matcher(first);
+        Pattern patterns = Pattern.compile(dotREGEX);
+        Matcher ms= patterns.matcher(second);
+        if(mf.find() || ms.find())
+        {
+            double dfirst = Double.parseDouble(first);
+            double dsecond = Double.parseDouble(second);
+            double sum = dfirst / dsecond;
+            sum = sum*100000;
+            if((sum*10)%10 >= 5 || (sum*10)%10 <= -5 && sum!=0) {
+                if(sum>0){sum++; }
+                else {sum--; }
+            }
+            int dec_sum = (int)sum;
+            sum = (double)dec_sum/100000;
+            if((sum*10)%10 == 0) {
+                int hold_sum = (int) sum;
+                St_sum = Integer.toString(hold_sum);
+            }
+            else {
+                St_sum = Double.toString(sum);
+            }
+        }
+        else {
+            int dfirst = Integer.parseInt(first);
+            int dsecond = Integer.parseInt(second);
+            int sum = dfirst / dsecond;
+            St_sum = Integer.toString(sum);
+        }
+
+
+        return St_sum;
     }
 
     public String power() {
-        return null;
+        String St_sum;
+        String first = firstOperand.getOperand();
+        String second = secondOperand.getOperand();
+
+        Pattern patternf = Pattern.compile(dotREGEX);
+        Matcher mf = patternf.matcher(first);
+        Pattern patterns = Pattern.compile(dotREGEX);
+        Matcher ms= patterns.matcher(second);
+        if(mf.find() || ms.find())
+        {
+            double dfirst = Double.parseDouble(first);
+            double dsecond = Double.parseDouble(second);
+            double sum = Math.pow(dfirst,dsecond);
+            sum = sum*100000;
+            if((sum*10)%10 >= 5 || (sum*10)%10 <= -5 && sum!=0) {
+                if(sum>0){sum++; }
+                else {sum--; }
+            }
+            int dec_sum = (int)sum;
+            sum = (double)dec_sum/100000;
+            if((sum*10)%10 == 0) {
+                int hold_sum = (int) sum;
+                St_sum = Integer.toString(hold_sum);
+            }
+            else {
+                St_sum = Double.toString(sum);
+            }
+        }
+        else {
+            int dfirst = Integer.parseInt(first);
+            int dsecond = Integer.parseInt(second);
+            double sum = Math.pow(dfirst,dsecond);
+            int hold_sum = (int) sum;
+            St_sum = Integer.toString(hold_sum);
+        }
+
+
+        return St_sum;
     }
 
 }
