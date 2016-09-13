4eb12c37ee5bceb9e7268d2ec46972133ee934b9
napornapon <na_hahaha@hotmail.com>
2016-09-13 15:58:53 +0700

    Update User.java

--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -1,28 +1,65 @@
 package cpe200;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
+    private static final String nameREGEX = "[A-z,0-9]";
+    private static final String nonREGEX = "[-,_,/,@,#,&]";
+    private static final String passazREGEX = "[a-z]";
+    private static final String passAZREGEX = "[A-Z]";
+    private static final String pass09REGEX = "[0-9]";
 
     public User() {
     }
 
     public boolean setUserName(String name) {
-        return false;
+        Pattern patternAz = Pattern.compile(nameREGEX);
+        Matcher mAz = patternAz.matcher(name);
+        Pattern patternsim = Pattern.compile(nonREGEX);
+        Matcher mSim = patternsim.matcher(name);
+        char c = name.charAt(0);
+        if(mAz.find() && name.length()>=8 && !(mSim.find())) {
+            if(c >= '0' && c <= '9')
+            {
+                return false;
+            }
+            else {
+                userName = name;
+                return true;
+            }
+        }
+        else
+            return false;
     }
 
     public boolean setPassword(String name) {
-        return false;
+        Pattern patternaz = Pattern.compile(passazREGEX);
+        Matcher maz = patternaz.matcher(name);
+        Pattern patternAZ = Pattern.compile(passAZREGEX);
+        Matcher mAZ = patternAZ.matcher(name);
+        Pattern pattern09 = Pattern.compile(pass09REGEX);
+        Matcher m09 = pattern09.matcher(name);
+        Pattern patternsim = Pattern.compile(nonREGEX);
+        Matcher mSim = patternsim.matcher(name);
+        if(maz.find()&& mAZ.find() && m09.find() &&!(mSim.find()) && name.length()>=12 ) {
+            password = name;
+            return true;
+        }
+        else
+            return false;
     }
 
     public String getUserName() {
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
